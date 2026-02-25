37bf48f33b552b2a3035cac39916adbb1bbe18e6
Joseph Geis <joseph@josephgeis.dev>
2026-02-25 14:35:02 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Thing Collection App
+
+I created this to demonstrate composition for my friend. Just a really simple React app.
---
+++ src/App.js
@@ -0,0 +1,103 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function Counter({ name, state }) {
+  const [counter, setCounter] = state;
+  const increment = e => setCounter(counter + 1);
+  const decrement = e => setCounter(Math.max(counter - 1, 0));
+
+  return (
+    <div>
+      <h1>{name}: {counter}</h1>
+      <button onClick={increment}>Increment {name}</button>
+      <button onClick={decrement}>Decrement {name}</button>
+    </div>
+  )
+}
+
+function Emoji({ emoji }) {
+  return <div style={{fontSize: "4em"}}>{emoji}</div>
+}
+
+function Dog() {
+  return <Emoji emoji="🐶" />
+}
+
+function Cat() {
+  return <Emoji emoji="🐱" />
+}
+
+function Collection({ Thing, number }) {
+  return (
+    <div style={{display: "flex", flexDirection: "row", justifyContent: "center", flexWrap: "wrap"}}>
+      {number > 0 ? [...Array(number)].map((_, index) => <Thing key={index} />) : <p>No items</p>}
+    </div>
+  )
+}
+
+function ThingDisplay({ name, Thing, startingNumber=3 }) {
+  const counterState = useState(startingNumber);
+  const [counter, setCounter] = counterState;
+
+  return (
+    <div>
+      <Counter name={name} state={counterState} />
+      <Collection Thing={Thing} number={counter} />
+    </div>
+  )
+}
+
+function AddThingForm({ addThing }) {
+  const [name, setName] = useState("");
+  const [emoji, _setEmoji] = useState("");
+
+  const isEmojiOnly = str => {
+    const emojiOnlyRegex = /^(\p{Extended_Pictographic})*$/ug;
+    return emojiOnlyRegex.test(str);
+  }
+  
+  const setEmoji = str => isEmojiOnly(str) ? _setEmoji(str) : null;
+
+  const onClickAdd = _ => {
+    addThing(name, emoji);
+    setName("");
+    setEmoji("");
+  }
+
+  return (
+    <div style={{display: "flex", gap: "1em"}}>
+      <div>
+        <label>Name</label>
+        <input value={name} onChange={e => setName(e.target.value)} />
+      </div>
+      <div>
+        <label>Emoji</label>
+        <input value={emoji} onChange={e => setEmoji(e.target.value)} maxLength={2} />
+      </div>
+      <button onClick={onClickAdd}>Add</button>
+    </div>
+  )
+}
+
+function App() {
+  const counterState = useState(0);
+  const [counter, setCounter] = counterState;
+  const [things, setThings] = useState([
+    {name: "Dog", thing: Dog},
+    {name: "Cat", thing: Cat}
+  ])
+
+  const addThing = (name, emoji) => {
+    setThings(things.concat([{name, thing: () => <Emoji emoji={emoji} /> }]))
+  }
+
+  return (
+    <div className="App">
+      <AddThingForm addThing={addThing} />
+      {things.map(({name, thing}) => <ThingDisplay name={name} Thing={thing} />)}
+    </div>
+  );
+}
+
+export default App;
